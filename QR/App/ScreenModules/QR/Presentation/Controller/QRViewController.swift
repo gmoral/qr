47448864e04e2ee4d3277188f4343615f1936b89
Guillermo Moral <willy.moral@gmail.com>
2023-04-27 10:11:14 -0300

    Add QR

---
+++ QR/App/ScreenModules/QR/Presentation/Controller/QRViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QRViewController.swift
+//  QR
+//
+//  Created by Guillermo Moral on 27/04/2023.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ QR/App/ScreenModules/QR/Presentation/Controller/QRViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QRViewModel.swift
+//  QR
+//
+//  Created by Guillermo Moral on 27/04/2023.
+//
+
+import Foundation
